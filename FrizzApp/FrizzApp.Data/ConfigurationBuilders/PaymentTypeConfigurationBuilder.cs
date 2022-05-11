482d369dfe0d632634ea17ba21d9dc85f778e97d
LuccaQuirino <quirinoluke@gmail.com>
2022-05-11 20:24:44 -0300

    Configuration Builders

---
+++ FrizzApp/FrizzApp.Data/ConfigurationBuilders/PaymentTypeConfigurationBuilder.cs
@@ -0,0 +1,20 @@
+﻿using FrizzApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FrizzApp.Data.ConfigurationBuilders
+{
+    public class PaymentTypeConfigurationBuilder : IEntityTypeConfiguration<PaymentType>
+    {
+        public void Configure(EntityTypeBuilder<PaymentType> builder)
+        {
+            builder.HasKey(x => x.PaymentTypeId);
+
+            builder.ToTable("PaymentTypes");
+
+            builder.Property(b => b.PaymentTypeName)
+                   .IsRequired(true)
+                   .HasMaxLength(20);
+        }
+    }
+}
