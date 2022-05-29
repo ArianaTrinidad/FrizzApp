9155e8eb210722e55ee61c2f5e4e0ab5e1d133bf
Julietadileo <julyetadl@gmail.com>
2022-05-29 14:28:24 -0300

    ResolverNombreLista

--- FrizzApp/FrizzApp.Api/Controllers/ProductController.cs
+++ FrizzApp/FrizzApp.Api/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
-﻿using FirzzApp.Business.Dtos.RequestDto;
+﻿using ClosedXML.Excel;
+using FirzzApp.Business.Dtos.RequestDto;
 using FirzzApp.Business.Interfaces.IServices;
 using FrizzApp.Api.ControllerSecurity;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace FrizzApp.Api.Controllers
 {
@@ -24,6 +26,49 @@ namespace FrizzApp.Api.Controllers
 
             return Ok(result);
         }
+
+
+
+        public IActionResult Index()
+        {
+            return Excel();
+        }
+
+        public IActionResult Excel()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("List<FrizzApp.Buisnes.Dtos.ResponseDto.GetProductResponseDto> result");
+                var currentRow = 1;
+                worksheet.Cell(currentRow, 1).Value = "Id";
+                worksheet.Cell(currentRow, 2).Value = "Nombre";
+                worksheet.Cell(currentRow, 3).Value = "Descripcion";
+                worksheet.Cell(currentRow, 4).Value = "Nota";
+                worksheet.Cell(currentRow, 5).Value = "Presentación";
+                worksheet.Cell(currentRow, 5).Value = "Precio";
+
+                foreach (var get in result)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = get.Id;
+                    worksheet.Cell(currentRow, 2).Value = get.Nombre;
+                    worksheet.Cell(currentRow, 3).Value = get.Descripcion;
+                    worksheet.Cell(currentRow, 4).Value = get.Nota;
+                    worksheet.Cell(currentRow, 5).Value = get.Presentación;
+                    worksheet.Cell(currentRow, 5).Value = get.Precio;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, 
+                                "application/vnd/openxmlformats-officedocument.spreadsheetml.sheet", 
+                                "ProductList.xlsx");
+                }
+            }
+        }
+
 
 
         [HttpPost]
