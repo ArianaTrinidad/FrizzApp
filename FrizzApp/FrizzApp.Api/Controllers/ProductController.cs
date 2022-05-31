313bf7c71d213eedeb3fdb2f2b36e12866dda872
claudiopedalino <claudio.dpedalino@gmail.com>
2022-05-30 23:41:53 -0300

    fix some things

--- FrizzApp/FrizzApp.Api/Controllers/ProductController.cs
+++ FrizzApp/FrizzApp.Api/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using ClosedXML.Excel;
 using System.IO;
 using System.Collections.Generic;
+using FirzzApp.Business.Dtos.ResponseDto;
 
 namespace FrizzApp.Api.Controllers
 {
@@ -23,50 +24,41 @@ namespace FrizzApp.Api.Controllers
         [HttpGet]
         public ActionResult GetAll([FromQuery] GetAllProductDto dto)
         {
-            var ProductList = _service.GetAll(dto);
+            var result = _service.GetAll(dto);
 
-            return Ok(ProductList);
+            return Ok(result);
         }
 
-        private List<CreateProductDto> ProductList = new List<CreateProductDto>
-        {
-            new CreateProductDto{Nombre = "Brownie",
-                Descripcion = "De chocolate",
-                Nota = "Sacar del freezer 5hs antes de consumir",
-                Presentacion = "3 unidades",
-                ImagenUrl = "InsertarURL",
-                Precio = 500,
-                EsPromo = true,
-                Categoria= 0},
-        };
 
-        private FileResult Excel()
+        [HttpGet("download")]
+        public FileResult Excel([FromQuery] GetAllProductDto dto)
         {
+            var result = _service.GetAll(dto);
+
             using (var workbook = new XLWorkbook())
             {
-
                 var worksheet = workbook.Worksheets.Add("ProductList");
                 var currentRow = 1;
-                worksheet.Cell(currentRow, 1).Value = "Nombre";
+                worksheet.Cell(currentRow, 1).Value = nameof(GetProductResponseDto.Nombre);
                 worksheet.Cell(currentRow, 2).Value = "Descripcion";
-                worksheet.Cell(currentRow, 2).Value = "Nota";
-                worksheet.Cell(currentRow, 2).Value = "Presentación";
-                worksheet.Cell(currentRow, 2).Value = "Imagen";
-                worksheet.Cell(currentRow, 3).Value = "Precio";
-                worksheet.Cell(currentRow, 2).Value = "Promo";
-                worksheet.Cell(currentRow, 2).Value = "Categoría";
+                worksheet.Cell(currentRow, 3).Value = "Nota";
+                worksheet.Cell(currentRow, 4).Value = "Presentación";
+                worksheet.Cell(currentRow, 5).Value = "Imagen";
+                worksheet.Cell(currentRow, 6).Value = "Precio";
+                worksheet.Cell(currentRow, 7).Value = "Promo";
+                worksheet.Cell(currentRow, 8).Value = "Categoría";
 
-                foreach (var get in ProductList)
+
+                foreach (var item in result)
                 {
                     currentRow++;
-                    worksheet.Cell(currentRow, 2).Value = get.Nombre;
-                    worksheet.Cell(currentRow, 3).Value = get.Descripcion;
-                    worksheet.Cell(currentRow, 5).Value = get.Nota;
-                    worksheet.Cell(currentRow, 5).Value = get.Presentacion;
-                    worksheet.Cell(currentRow, 5).Value = get.ImagenUrl;
-                    worksheet.Cell(currentRow, 5).Value = get.Precio;
-                    worksheet.Cell(currentRow, 5).Value = get.EsPromo;
-                    worksheet.Cell(currentRow, 5).Value = get.Categoria;
+                    worksheet.Cell(currentRow, 1).Value = item.Nombre;
+                    worksheet.Cell(currentRow, 2).Value = item.Descripcion;
+                    worksheet.Cell(currentRow, 3).Value = item.Nota;
+                    worksheet.Cell(currentRow, 4).Value = item.Presentacion;
+                    worksheet.Cell(currentRow, 5).Value = item.ImagenUrl;
+                    worksheet.Cell(currentRow, 6).Value = item.Precio;
+                    worksheet.Cell(currentRow, 7).Value = item.EsPromo == true ? "Tiene stock" : "No tiene stock";
                 }
 
                 using (var stream = new MemoryStream())
@@ -74,17 +66,11 @@ namespace FrizzApp.Api.Controllers
                     workbook.SaveAs(stream);
                     var content = stream.ToArray();
                     return File(content,
-                                "application/vnd/openxmlformats-officedocument.spreadsheetml.sheet",
+                                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                                 "ProductList.xlsx");
                 }
             }
         }
-
-        private IActionResult Index()
-        {
-            return Excel();
-        }
-
 
 
         [HttpPost]
