7c652521cd83ca5f842ea9c2c1b8870492b375e7
Julietadileo <julyetadl@gmail.com>
2022-07-13 19:25:17 -0300

    NoConfigExtryTime

--- FrizzApp/FirzzApp.Business/Interfaces/ICacheService.cs
+++ FrizzApp/FirzzApp.Business/Interfaces/ICacheService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
 using System;
 
 namespace FirzzApp.Business.Interfaces
@@ -7,7 +6,7 @@ namespace FirzzApp.Business.Interfaces
     public interface ICacheService
     {
         TEntry Get<TEntry, TCacheDtoConfiguration>(string key, TCacheDtoConfiguration dtoConfig) where TCacheDtoConfiguration : ICacheable;
-        void Remove(string key);
-        bool Set<TValue>(string key, TValue dataToCache, TimeSpan? absoluteExpireTime = null);
+        bool Remove(string key);
+        bool Set<TValue>(string key, TValue dataToCache); //DateTimeOffset expirationTime
     }
 }
--- FrizzApp/FirzzApp.Business/Services/CacheService.cs
+++ FrizzApp/FirzzApp.Business/Services/CacheService.cs
@@ -11,20 +11,14 @@ namespace FirzzApp.Business.Services
 {
     public class CacheService : ICacheService
     {
-        private readonly IDistributedCache _cache;
         private readonly IConfiguration _configuration;
         private readonly IDatabase _db;
 
-        //public CacheService(IDistributedCache cache, IConfiguration configuration)
-        //{
-        //    _cache = cache;
-        //    _configuration = configuration;
-        //}
 
         public CacheService(IConfiguration configuration)
         {
             _configuration = configuration;
-            
+
             var connectionString = _configuration.GetValue<string>("CacheConfiguration:ConnectionStrings:Redis");
             var redis = ConnectionMultiplexer.Connect(connectionString);
             _db = redis.GetDatabase();
@@ -33,14 +27,12 @@ namespace FirzzApp.Business.Services
         public TEntry Get<TEntry, TCacheDtoConfiguration>(string key, TCacheDtoConfiguration dtoConfig)
             where TCacheDtoConfiguration : ICacheable
         {
-            //TEntry entry = default;
             var value = _db.StringGet(key);
 
             var cacheConfigStatus = _configuration.GetValue<bool>("CacheConfiguration:UseCache");
 
-            if (dtoConfig.CacheType == CacheTypeEnum.UseCache && cacheConfigStatus == true)
+            if (dtoConfig.CacheType == CacheTypeEnum.UseCache && cacheConfigStatus == true && value.HasValue)
             {
-                //_cache.TryGetValue(key, out entry);
                 return JsonConvert.DeserializeObject<TEntry>(value);
             }
 
@@ -48,19 +40,23 @@ namespace FirzzApp.Business.Services
         }
 
 
-        public bool Set<TValue>(string key, TValue dataToCache, TimeSpan? absoluteExpireTime = null)
+        public bool Set<TValue>(string key, TValue dataToCache) //DateTimeOffset expirationTime
         {
-            var options = new DistributedCacheEntryOptions();
-            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(600);
+            //TimeSpan expiryTime = expirationTime.TimeSpan.FromSeconds(600);
 
             var isSet = _db.StringSet(key, JsonConvert.SerializeObject(dataToCache));
             return isSet;
         }
 
-        public void Remove(string key)
+        public bool Remove(string key)
         {
-            _cache.Remove(key);
-        }
+            bool _isKeyExist = _db.KeyExists(key);
+            if (_isKeyExist == true)
+            {
+                return _db.KeyDelete(key);
+            }
+            return false;
 
+        }
     }
 }
